fb31bc7bd43daaac1b3e78b5cd73ad488b881635
Kreto <mateusz.kret@zse.krakow.pl>
2023-04-02 11:47:01 +0200

    Standard and Ranked grade books

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
+
