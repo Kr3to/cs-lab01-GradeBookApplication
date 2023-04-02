4034e80b8cdaaabe5a388f4fadd060fc7157c04d
Kreto <mateusz.kret@zse.krakow.pl>
2023-04-02 12:03:34 +0200

    Override GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
 ﻿using GradeBook.Enums;
 using GradeBook.GradeBooks;
+using System.Linq;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +10,40 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            // Determine the threshold grade to earn an 'A'
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+            // Check the input grade against the thresholds for each letter grade
+            if (averageGrade >= grades[threshold - 1])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= grades[(threshold * 2) - 1])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= grades[(threshold * 3) - 1])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= grades[(threshold * 4) - 1])
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
